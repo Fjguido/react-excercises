abcde2da41e07cbbb0868b02f8b574e533ff65c3
Francis Guido <fjguido97@gmail.com>
2022-08-18 14:17:53 -0400

    added delete function

--- reactpractice/src/components/FeedbackItem.jsx
+++ reactpractice/src/components/FeedbackItem.jsx
@@ -3,11 +3,14 @@ import Card from "./shared/Card"
 import PropType from 'prop-types'
 
 
-function FeedbackItem({item}) {
+
+function FeedbackItem({item, handleDelete}) {
+
+
   return (
     <Card >
          <div className="num-display">{item.rating}</div>
-         <button className="close">
+         <button onClick={() => handleDelete(item.id)} className="close">
           <FaTimes color='purple' />
          </button>
         <div className="text-display">{item.text}</div>
