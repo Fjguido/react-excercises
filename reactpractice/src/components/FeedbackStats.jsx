fe5f6e3bd1041ae94afd5a8f7072e886d4b820c5
Francis Guido <fjguido97@gmail.com>
2022-08-19 12:14:43 -0400

    added avg of stats

--- reactpractice/src/components/FeedbackStats.jsx
+++ reactpractice/src/components/FeedbackStats.jsx
@@ -1,10 +1,17 @@
 
 
 function FeedbackStats({feedback}) {
+// Calculate ratings avg
+
+ let average = feedback.reduce((acc,cur) => {
+    return acc + cur.rating
+ }, 0) / feedback.length
+console.log(average)
+
   return (
     <div className="feedback-stats">
         <h4>{feedback.length} Reviews</h4>
-        <h4>Average Rating: 10</h4>
+        <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
     </div>
   )
 }
