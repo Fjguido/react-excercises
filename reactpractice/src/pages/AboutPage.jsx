7bac0a52db7a6e5a863abc32bc1a98229a170bc2
Francis Guido <fjguido97@gmail.com>
2022-08-25 17:01:03 -0400

    added about card

--- reactpractice/src/pages/AboutPage.jsx
+++ reactpractice/src/pages/AboutPage.jsx
@@ -1,9 +1,13 @@
 import {Link} from 'react-router-dom'
-
+import Card from '../components/shared/Card'
 function AboutPage() {
-  return (
-    <div>AboutPage</div>
-  )
+  return <Card>
+    <div className="about">
+        <h1>About this Project</h1>
+        <p>React project to post feedback for a product</p>
+    </div>
+
+  </Card> 
 }
 
 export default AboutPage
