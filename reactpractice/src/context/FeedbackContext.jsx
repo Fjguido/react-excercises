98e3d1d4dcf1bc434e4e4da8b41ceee9dcc34113
Francis Guido <fjguido97@gmail.com>
2022-09-12 14:40:21 -0400

    added notes for context/provider

--- reactpractice/src/context/FeedbackContext.jsx
+++ reactpractice/src/context/FeedbackContext.jsx
@@ -1,8 +1,8 @@
 import {createContext, useState} from 'react'
 
 const FeedbackContext = createContext()
-// now create a provider 
 
+// now create a provider 
 export const FeedbackProvider = ({children}) => {
 const [feedback, setFeedback] = useState([
     {
@@ -16,10 +16,13 @@ const [feedback, setFeedback] = useState([
         feedback,
 
     }}>
+
         {children}
 
     </FeedbackContext.Provider>
     
 }
-
+// youre returning whatever you wrapped, and eventually all the 
+// components that need access to the context 
+// any value you want to pass in, such as state/value - you pass it down to the value
 export default FeedbackContext
