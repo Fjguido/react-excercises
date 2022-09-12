a76d2887eb558dee727187cfb892cc2959d1b507
Francis Guido <fjguido97@gmail.com>
2022-09-12 13:09:36 -0400

    added link for home/about page

--- reactpractice/src/pages/AboutPage.jsx
+++ reactpractice/src/pages/AboutPage.jsx
@@ -1,10 +1,14 @@
 import {Link} from 'react-router-dom'
 import Card from '../components/shared/Card'
 function AboutPage() {
-  return <Card>
+
+  return  <Card>
     <div className="about">
         <h1>About this Project</h1>
         <p>React project to post feedback for a product</p>
+        <p>
+          <Link to='/'>Back To Home</Link>
+        </p>
     </div>
 
   </Card> 
