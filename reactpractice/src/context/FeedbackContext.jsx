9b125a160aebaa4fd437800f2c9ad7131d391587
Francis Guido <fjguido97@gmail.com>
2022-09-12 17:10:35 -0400

    made edit feature  global

--- reactpractice/src/context/FeedbackContext.jsx
+++ reactpractice/src/context/FeedbackContext.jsx
@@ -23,22 +23,41 @@ const [feedback, setFeedback] = useState([
     }
 ])
 
+
+const [feedbackEdit, setFeedbackEdit] = useState ({
+    item: {},
+    edit: false
+})
+
+//delete feedback
 const deleteFeedback = (id) => {
     if(window.confirm('Are you sure you want to delete?')) {
         setFeedback(feedback.filter((item) => item.id !== id))
     }
 }
 
+// add feedback
 const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4()
     setFeedback([newFeedback, ...feedback])
 
 }
 
+// Set item to be updated
+const editFeedback = (item) => {
+    setFeedbackEdit(
+        {
+            item,
+            edit: true
+        }
+    )
+}
+
     return <FeedbackContext.Provider value={{
         feedback,
         deleteFeedback,
         addFeedback,
+        editFeedback,
 
     }}>
 
